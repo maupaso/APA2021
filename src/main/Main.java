9c4d815b555e4b2b5432bb26feb2a9a1c4ea2bbe
Marcos Aurelio de Paiva Souza <marcossouza@br.ibm.com>
2021-06-26 03:15:33 -0300

    Ajustes identação

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,77 +6,77 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		int nodeNumber = 0;
 		int edgeNumber = 0;
-		
+
 		String inputType = "";
 
-	    try {
-	        File myObj = new File("src/resources/test_set1/check_v5_s1.dat");
-//	        File myObj = new File("src/resources/DMXA/dmxa0296.stp");
-	        Scanner myReader = new Scanner(myObj);
+		try {
+			File myObj = new File("src/resources/test_set1/check_v5_s1.dat");
+			//	        File myObj = new File("src/resources/DMXA/dmxa0296.stp");
+			Scanner myReader = new Scanner(myObj);
 
-	        while(myReader.hasNext()) {
-	        	if(myReader.hasNext("NB_NODES")) {
-	        		inputType = "testset";
-	        		myReader.next("NB_NODES");
-	        		nodeNumber = myReader.nextInt();
-	        	} else if(myReader.hasNext("NB_ARCS")) {
-	        		inputType = "testset";
-	        		myReader.next("NB_ARCS");
-	        		edgeNumber = myReader.nextInt();
-	        	} else if(myReader.hasNext("Nodes")) {
-	        		inputType = "data";
-	        		myReader.next("Nodes");
-	        		nodeNumber = myReader.nextInt();
-	        	} else if(myReader.hasNext("Edges")) {
-	        		inputType = "data";
-	        		myReader.next("Edges");
-	        		edgeNumber = myReader.nextInt();	
-	        	} else {
-	        		myReader.nextLine();
-	        	}
-	        	
-	        	if(nodeNumber > 0 && edgeNumber > 0) {
-	        		break;
-	        	}
-	        }
-        	
-        	if(inputType == "testset") {
-        		myReader.nextLine();
-        		myReader.nextLine();
-        	}
-        	
-        	for(int i = 0; i < edgeNumber; i++) {
-        		if(inputType == "data") {
-        			//Descarta identificador "E"
-        			myReader.next();
-        		}
-        		
-        		if(myReader.hasNextInt()) {
-	        		int startNode = myReader.nextInt();
-	        		int endNode = myReader.nextInt();
-	        		int cost = myReader.nextInt();
-	        		
-	        		System.out.println(startNode + " --> " + endNode + " - CUSTO: " + cost);
-        		}
-        		
-        	}
+			while(myReader.hasNext()) {
+				if(myReader.hasNext("NB_NODES")) {
+					inputType = "testset";
+					myReader.next("NB_NODES");
+					nodeNumber = myReader.nextInt();
+				} else if(myReader.hasNext("NB_ARCS")) {
+					inputType = "testset";
+					myReader.next("NB_ARCS");
+					edgeNumber = myReader.nextInt();
+				} else if(myReader.hasNext("Nodes")) {
+					inputType = "data";
+					myReader.next("Nodes");
+					nodeNumber = myReader.nextInt();
+				} else if(myReader.hasNext("Edges")) {
+					inputType = "data";
+					myReader.next("Edges");
+					edgeNumber = myReader.nextInt();	
+				} else {
+					myReader.nextLine();
+				}
 
-        	
-        	
+				if(nodeNumber > 0 && edgeNumber > 0) {
+					break;
+				}
+			}
 
-        	myReader.close();
-	      } catch (FileNotFoundException e) {
-	        System.out.println("An error occurred.");
-	        e.printStackTrace();
-	      }
-		
-	    System.out.println("TIPO DE ENTRADA: " + inputType);
-	    System.out.println("NÚMERO DE NÓS: " + nodeNumber);
-	    System.out.println("NÚMERO DE ARESTAS: " + edgeNumber);
-		
+			if(inputType == "testset") {
+				myReader.nextLine();
+				myReader.nextLine();
+			}
+
+			for(int i = 0; i < edgeNumber; i++) {
+				if(inputType == "data") {
+					//Descarta identificador "E"
+					myReader.next();
+				}
+
+				if(myReader.hasNextInt()) {
+					int startNode = myReader.nextInt();
+					int endNode = myReader.nextInt();
+					int cost = myReader.nextInt();
+
+					System.out.println(startNode + " --> " + endNode + " - CUSTO: " + cost);
+				}
+
+			}
+
+
+
+
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+		System.out.println("TIPO DE ENTRADA: " + inputType);
+		System.out.println("NÚMERO DE NÓS: " + nodeNumber);
+		System.out.println("NÚMERO DE ARESTAS: " + edgeNumber);
+
 	}
 
 }
